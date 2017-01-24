9c8f27c025f1929433a7e4a6b91a0c2bb03496da
persona <personaprog@gmail.com>
2017-01-24 20:58:33 -0200

    Atualizando ProjAndroid

--- programacao/app/src/main/java/com/example/persprog/programacao/MainActivity.java
+++ programacao/app/src/main/java/com/example/persprog/programacao/MainActivity.java
@@ -2,18 +2,17 @@ package com.example.persprog.programacao;
 
 
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
+import android.widget.ArrayAdapter;
 import com.example.persprog.programacao.ca4.Bundle_Exemp;
-import com.example.persprog.programacao.ca4.Lifecycle;
-import com.example.persprog.programacao.cap3.Layout;
 import com.example.persprog.programacao.cap6.ActFrameLayout;
 import com.example.persprog.programacao.cap6.ActGridLayout;
 import com.example.persprog.programacao.cap6.ActLinearLayout;
@@ -22,134 +21,60 @@ import com.example.persprog.programacao.cap6.ActScrollView;
 import com.example.persprog.programacao.cap6.ActTabelLayout1;
 import com.example.persprog.programacao.cap6.ActTableLayout;
 
-public class MainActivity extends Lifecycle {
+public class MainActivity extends ListActivity {
+
     private  final Context context = this;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.layoutlista);
-        LayoutInflater inflater = LayoutInflater.from(this);
+        String[] items = new String[] {"Bundle","FrameLayout","LinearLayout","TableLayout shrinkColumns","TableLayout strechColumns","GridLayout","RelativeLayout","ScrollView", "Sair"};
+        this.setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items));
+    }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        try {
+            switch (position) {
+                case 0:
 
-        Button btnlayout =  (Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btnlayout.setText("Layout");
-        btnlayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent activelayout = new Intent(context, Layout.class);
-                startActivity(activelayout);
+                    Intent itbundle = new Intent(this, Bundle_Exemp.class);
+                    Bundle params = new Bundle();
+                    params.putString("nome", "Personaprog");
+                    itbundle.putExtras(params);
+                    startActivity(itbundle);
+
+                    break;
+                case 1:
+                    startActivity(new Intent(this,ActFrameLayout.class));
+                    break;
+                case 2:
+                    startActivity(new Intent(this,ActLinearLayout.class));
+                    break;
+                case 3:
+                    startActivity(new Intent(this,ActTableLayout.class));
+                    break;
+                case 4:
+                    startActivity(new Intent(this,ActTabelLayout1.class));
+                    break;
+                case 5:
+                    startActivity(new Intent(this,ActGridLayout.class));
+                    break;
+                case 6:
+                    startActivity(new Intent(this,ActRelativeLayout.class));
+                    break;
+                case 7:
+                    startActivity(new Intent(this,ActScrollView.class));
+                    break;
+                case 8:
+                   finish();
+                    break;
+                default:
+                    finish();
+                    break;
             }
-        });
-        layout.addView(btnlayout);
-
-
-        Button btnbundle = (Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btnbundle.setText("Bundle");
-        btnbundle.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activebundle = new Intent(context, Bundle_Exemp.class);
-
-                Bundle params = new Bundle();
-                params.putString("nome", TAG);
-                activebundle.putExtras(params);
-                startActivity(activebundle);
-            }
-        });
-        layout.addView(btnbundle);
-
-        Button btnframelayout =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btnframelayout.setText("Frame Layout");
-        btnframelayout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActFrameLayout.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btnframelayout);
-
-
-        Button btnlinearlayout =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btnlinearlayout.setText("Linear Layout");
-        btnlinearlayout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActLinearLayout.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btnlinearlayout);
-
-
-        Button btntablelayout =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btntablelayout.setText("TableLayout shrinkColumns");
-        btntablelayout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActTableLayout.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btntablelayout);
-
-        Button btntablelayout1 =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btntablelayout1.setText("TableLayout strechColumns");
-        btntablelayout1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActTabelLayout1.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btntablelayout1);
-
-
-        Button btngridlayout =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btngridlayout.setText("GridLayout");
-        btngridlayout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActGridLayout.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btngridlayout);
-
-
-
-        Button btnrelativelayout =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btnrelativelayout.setText("RelativeLayout");
-        btnrelativelayout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActRelativeLayout.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btnrelativelayout);
-
-
-
-
-
-
-        Button btnscrollview =(Button)  inflater.inflate(R.layout.inflatebutton, layout,false);
-        btnscrollview.setText("GridLayout");
-        btnscrollview.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent activeInt = new Intent(context, ActScrollView.class);
-                startActivity(activeInt);
-            }
-        });
-        layout.addView(btnscrollview);
-
-
-
-
-
+        } catch (Exception e) {
+            Toast.makeText(this, "Erro :" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
 
