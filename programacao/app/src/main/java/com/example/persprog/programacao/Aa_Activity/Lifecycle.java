e90c6c6ca24afe4f6e94bd977a13180cce3b8c8b
persona <personaprog@gmail.com>
2017-01-26 11:04:12 -0200

    Atualizando ProjAndroid

--- programacao/app/src/main/java/com/example/persprog/programacao/Aa_Activity/Lifecycle.java
+++ programacao/app/src/main/java/com/example/persprog/programacao/Aa_Activity/Lifecycle.java
@@ -1,9 +1,12 @@
 package com.example.persprog.programacao.Aa_Activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by persprog on 21/01/17.
@@ -57,8 +60,27 @@ public class Lifecycle extends AppCompatActivity {
         int id = item.getItemId();
 
         if(id == android.R.id.home){
-            finish();
+         /*   AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Activitys");
+            builder.setMessage("Fechar Aplicativos?");
+            builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {*/
+                    finish();
+
+         /*       }
+            });
+            builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getBaseContext(),"Acrivitty Diponivel",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();*/
             return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
